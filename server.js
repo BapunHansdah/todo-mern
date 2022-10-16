6addcfe7cb1bb8db29eeb8cd75f2d88513f72784
Bapun <bapunhansdah777@gmail.com>
2022-10-16 15:28:39 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+
+dotenv.config()
 const port = process.env.PORT || 4000
-const app = express()
 
-// app.get("/",(res,req)=>console.log("hello world"))
+const app = express()
 
 app.use('/', (req, res) => {
     res.send('Hello World, from express');
