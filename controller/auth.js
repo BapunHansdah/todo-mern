b79cb2b41d9c3321800aec19a7207922e225d93f
Bapun <bapunhansdah777@gmail.com>
2022-10-17 08:32:12 +0530

    login route and controller created

--- controller/auth.js
+++ controller/auth.js
@@ -30,4 +30,35 @@ export const register = async (req,res,next) =>{
 	}
 }
 
+export const login = async (req,res,next) =>{
+	const {email,password} = req.body
+	if(!email || !password){
+		return res.status(403).json('inputs cant be empty')
+	}
+	try{
+		const user =await User.findOne({email}).select('name email password')
+
+		if(!user){
+			return res.status(404).json("the user does not exist")
+		}
+		const checkPassword = await bcrypt.compare(password,user.password) 
+		if(!checkPassword){
+			return res.status(401).json('password is incorrect')
+		}
+		const payload ={
+			id:user._id,
+			name:user.name
+		}
+		const token = jwt.sign(payload,process.env.JWT_SECRET,{
+			expiresIn:'1d'
+		})
+
+		return res.cookie('access_token',token ,{httpOnly:true}).status(200).json({name:user.name,email:user.email,message:"login success"});
+
+	}
+	catch(err){
+		return next(err)
+	}
+
+}
 
